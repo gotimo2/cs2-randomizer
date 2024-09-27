b875a4679f41c4763f5e3744726c19be514d2148
gotimo2 <timost1402@hotmail.com>
2024-09-27 20:06:37 +0200

    moved randomizer to Randomizer class

--- cs2-randomizer/Events.cs
+++ cs2-randomizer/Events.cs
@@ -12,8 +12,11 @@ namespace cs2_randomizer
     public class Events
     {
         private readonly ILogger _logger;
+        private readonly Randomizer randomizer;
+
         public Events(ILogger logger) { 
             _logger = logger;
+            randomizer = new Randomizer(logger);
         }
 
         [GameEventHandler]
@@ -22,63 +25,11 @@ namespace cs2_randomizer
             _logger.LogDebug("Round start");
             if (Plugin.RandomizerEnabled)
             {
-                RandomizeLoadouts();
+                randomizer.RandomizeLoadouts();
             }
             return HookResult.Continue;
         }
 
-        private void RandomizeLoadouts()
-        {
-            var players = Utils.GetAllPlayers();
-            var random = new Random();
-            var weapons = Utils.Weapons;
 
-            foreach (var player in players)
-            {
-                player.RemoveWeapons();
-
-                if (player.TeamNum == 3) //CT side
-                {
-                    if (random.Next(0, 2) == 1)
-                    {
-                        giveDefuser(player);
-                    }
-                }
-                if (player.TeamNum == 2) //T side
-                {
-                    if (random.Next(0, 2) == 1){
-                        player.GiveNamedItem(Constants.bomb_weaponName);
-                    }
-                } 
-                var weapon = weapons[random.Next(weapons.Count - 1)];
-                RollArmor(player);
-                player.GiveNamedItem(weapon);
-                player.GiveNamedItem(Constants.knife_weaponName);
-            }
-
-        }
-
-        private void giveDefuser(CCSPlayerController playerController)
-        {
-            var itemServices = new CCSPlayer_ItemServices(playerController.Pawn.Value.ItemServices.Handle);
-            itemServices.HasDefuser = true;
-        }
-
-        private void RollArmor(CCSPlayerController player)
-        {
-            Random random = new();
-            int result = random.Next(0, 3);
-            switch (result)
-            {
-                case 0:
-                    return;
-                case 1:
-                    player.GiveNamedItem(Constants.armor_weaponName);
-                    return;
-                case 2:
-                    player.GiveNamedItem(Constants.armorHelmet_weaponName);
-                    return;
-            }
-        }
     }
 }
