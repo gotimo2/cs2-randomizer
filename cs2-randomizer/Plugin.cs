c5f033264a0eef3c3bd56b3104c5292573a04f1c
gotimo2 <timost1402@hotmail.com>
2024-09-27 17:59:12 +0200

    added weapons / plugin boilerplate

--- cs2-randomizer/Plugin.cs
+++ cs2-randomizer/Plugin.cs
@@ -1,6 +1,15 @@
 ﻿namespace cs2_randomizer;
+using CounterStrikeSharp;
+using CounterStrikeSharp.API.Core;
 
-public class Plugin
+public class Plugin : BasePlugin
 {
+    public override string ModuleName => "CS2 Randomizer";
 
+    public override string ModuleVersion => "0.5";
+
+    public void OnLoad()
+    {
+
+    }
 }
---
+++ cs2-randomizer/Utils.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs2_randomizer
+{
+    public static class Utils
+    {
+        public static List<string> Weapons { get => new List<string>{
+            "weapon_m4a4",
+            "weapon_m4a1_silencer",
+            "weapon_famas",
+            "weapon_ak47",
+            "weapon_galilar",
+            "weapon_sg556",
+            "weapon_scar20",
+            "weapon_awp",
+            "weapon_ssg08",
+            "weapon_g3sg1",
+            "weapon_mp9",
+            "weapon_mp7",
+            "weapon_mp5sd",
+            "weapon_ump45",
+            "weapon_p90",
+            "weapon_bizon",
+            "weapon_mac10",
+            "weapon_usp_silencer",
+            "weapon_hkp2000",
+            "weapon_glock",
+            "weapon_elite",
+            "weapon_p250",
+            "weapon_fiveseven",
+            "weapon_cz75a",
+            "weapon_tec9",
+            "weapon_revolver",
+            "weapon_deagle",
+            "weapon_nova",
+            "weapon_xm1014",
+            "weapon_mag7",
+            "weapon_sawedoff",
+            "weapon_m249",
+            "weapon_negev",
+            "weapon_taser"
+            }; }
+    }
+}
