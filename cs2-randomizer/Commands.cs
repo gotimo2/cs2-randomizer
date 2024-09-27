df8e8fd3a1a7ae498daa1e2542b288de4cd47151
gotimo2 <timost1402@hotmail.com>
2024-09-28 01:31:55 +0200

    added commandHelper

--- cs2-randomizer/Commands.cs
+++ cs2-randomizer/Commands.cs
@@ -18,15 +18,27 @@ namespace cs2_randomizer
         }
 
         [ConsoleCommand("enable_randomizer", "Enables the randomizer plugin")]
+        [CommandHelper(minArgs: 0, usage: "enable_randomizer", whoCanExecute: CommandUsage.SERVER_ONLY)]
         public void EnableRandomizer(CCSPlayerController? player, CommandInfo commandInfo)
         {
+            if (player is not null)
+            {
+                return;
+            }
+
             Plugin.RandomizerEnabled = true;
             _logger.LogInformation("Randomizer enabled!");
         }
 
         [ConsoleCommand("disble_randomizer", "Disables the randomizer plugin")]
+        [CommandHelper(minArgs: 0, usage: "disable_randomizer", whoCanExecute: CommandUsage.SERVER_ONLY)]
         public void DisableRandomizer(CCSPlayerController? player, CommandInfo commandInfo)
         {
+            if (player is not null)
+            {
+                return;
+            }
+
             Plugin.RandomizerEnabled = false;
             _logger.LogInformation("Randomizer Disabled!");
         }
