afcf778565bd3f6a6fa6aaf6e3f5dcea20a6e573
gotimo2 <timost1402@hotmail.com>
2024-09-27 18:13:48 +0200

    split commands to seperate class

---
+++ cs2-randomizer/Commands.cs
@@ -0,0 +1,27 @@
+﻿using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs2_randomizer
+{
+    internal class Commands
+    {
+        [ConsoleCommand("enable_randomizer", "Enables the randomizer plugin")]
+        [CommandHelper((int)CommandUsage.SERVER_ONLY)]
+        public void EnableRandomizer()
+        {
+            Plugin.RandomizerEnabled = true;
+        }
+
+        [ConsoleCommand("disble_randomizer", "Disables the randomizer plugin")]
+        [CommandHelper((int)CommandUsage.SERVER_ONLY)]
+        public void DisableRandomizer()
+        {
+            Plugin.RandomizerEnabled = false;
+        }
+    }
+}
--- cs2-randomizer/Plugin.cs
+++ cs2-randomizer/Plugin.cs
@@ -9,24 +9,13 @@ public class Plugin : BasePlugin
 
     public override string ModuleVersion => "0.5";
 
-    private bool _enabled = false;
+    public static bool RandomizerEnabled = false;
 
     public override void Load(bool hotReload)
     {
         Console.WriteLine($"CS2 Randomizer {(hotReload ? "hot " : "")}Loaded !");
     }
 
-    [ConsoleCommand ("enable_randomizer", "Enables the randomizer plugin")]
-    [CommandHelper((int)CommandUsage.SERVER_ONLY)]
-    public void EnableRandomizer()
-    {
-        _enabled = true;
-    }
 
-    [ConsoleCommand("disble_randomizer", "Disables the randomizer plugin")]
-    [CommandHelper((int)CommandUsage.SERVER_ONLY)]
-    public void DisableRandomizer()
-    {
-        _enabled = true;
-    }
+
 }
